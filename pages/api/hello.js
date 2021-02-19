a9c1aa65a47246d3e711e1c074ff1dc4e7bb2df8
João Fernando Zanato <jfzanato@hotmail.com>
2021-02-19 12:09:01 -0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+  
+const app = require('express')()
+const { v4 } = require('uuid')
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+})
+
+app.get('/item/:slug', (req, res) => {
+  const { slug } = req.params
+  res.end(`Item: ${slug}`)
+})
+
+module.exports = app
